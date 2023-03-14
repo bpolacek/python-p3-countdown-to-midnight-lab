d06b987da7039ce5324affb2d66a3627b6ebe2f1
Brett Polacek <brettpolacek@gmail.com>
2023-03-14 14:24:12 -0400

    added functions

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,17 @@
-# your code goes here!
+import time
+
+def countdown(x):
+    while x > 0:
+        print(f"{x} SECOND(S)!")
+        x-=1
+    if x == 0:
+        print("HAPPY NEW YEAR!")
+
+def countdown_with_sleep(number):
+    while number>0:
+        print(f"{number} SECOND(S)!")
+        number-=1
+        time.sleep(1)
+    if number == 0:
+        print("HAPPY NEW YEAR!")
+    
